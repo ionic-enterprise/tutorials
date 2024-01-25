1b14f8cbac0508647b055354118b0f175890c66e
Eric Horodyski <horodyski@ionic.io>
2024-01-25 11:48:01 -0500

    feat: update to Ionic Docs Preset

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,63 +1,84 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const { themes } = require('prism-react-renderer');
+const { remarkCodeHike } = require('@code-hike/mdx');
 
-const { remarkCodeHike } = require("@code-hike/mdx");
-
-/** @type {import('@docusaurus/types').Config} */
-const config = {
+module.exports = {
   title: 'Ionic Enterprise Tutorials',
-  favicon: 'img/favicon-96x96.png',
-
-  url: 'https://ionic-enterprise.github.io',
-  baseUrl: '/docs/tutorials',
-  organizationName: 'ionic-enterprise',
-  projectName: 'tutorials',
+  url: 'https://ionic.io',
+  trailingSlash: false,
+  baseUrl: '/docs/tutorials/',
+  baseUrlIssueBanner: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+  favicon: 'img/favicon-96x96.png',
+  organizationName: 'ionic-enterprise',
+  projectName: 'tutorials',
+  titleDelimiter: '-',
+  themeConfig: {
+    logo: {
+      alt: 'Ionic Enterprise Tutorials',
+      src: 'img/logo.svg',
+      srcDark: 'img/favicon-96x96.png',
+      href: '/docs/tutorials',
+      height: 24,
+      width: 80,
+    },
+    sidebar: {
+      productDropdown: {
+        title: 'Enterprise Tutorials',
+        logo: {
+          width: 20,
+          height: 20,
+          alt: 'Enterprise Tutorials',
+          src: 'img/favicon-96x96.png',
+        },
+      },
+      backButton: {
+        url: {
+          href: '/docs',
+        },
+      },
+    },
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    prism: {
+      theme: themes.github,
+      darkTheme: themes.dracula,
+      additionalLanguages: ['shell-session', 'kotlin', 'groovy', 'java', 'swift', 'ruby', 'json', 'bash'],
+    },
+    zoom: {
+      selector: '.markdown em > img',
+      background: {
+        light: 'var(--token-background-color)',
+        dark: 'var(--token-background-color)',
+      },
+      config: {
+        margin: 75,
+        scrollOffset: 20,
+      },
+    },
   },
-
+  plugins: ['docusaurus-plugin-image-zoom'],
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      '@ionic-docs/preset-classic',
+      {
         docs: {
           beforeDefaultRemarkPlugins: [[remarkCodeHike, { lineNumbers: true }]],
-          routeBasePath: '/',
+          routeBasePath: '/docs/tutorials',
           sidebarPath: require.resolve('./sidebars.js'),
+          breadcrumbs: false,
         },
+        pages: false,
         theme: {
           customCss: [
             require.resolve("@code-hike/mdx/styles.css"),
-            require.resolve('./src/css/custom.css')
-          ]
+            require.resolve('./src/styles/custom.css')
+          ],
         },
-      }),
+      },
     ],
   ],
-  themes: ['mdx-v2'],
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Enterprise Tutorials',
-        logo: {
-          alt: 'Ionic Enterprise Tutorials',
-          src: 'img/logo.svg',
-        }
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
 };
-
-module.exports = config;
